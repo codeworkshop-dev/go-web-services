2537e46f07a207f63bcd9ae5b98492ed3bb7e75a
Stephen Castle <stephen@callsignmedia.com>
2020-01-21 17:15:04 -0800

    skeleton app

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/codeworkshop-dev/go-web-services
 
 go 1.13
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/gorilla/mux v1.7.3
+	golang.org/x/sys v0.0.0-20200121082415-34d275377bf9 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,116 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world")
-	})
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	r := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Println("listening on", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
 }
+
+/*
+REST Handlers
+*/
+
+// TweetList returns a list of tweet structs.
+func TweetList(w http.ResponseWriter, r *http.Request) {
+	todos := Tweets{
+		Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Some big polar bears hear at the north pole today"},
+		Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Saw over 5000 penguins, nothing new to report."},
+	}
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+}
+
+//TweetDetail returns a single tweet.
+func TweetDetail(w http.ResponseWriter, r *http.Request) {
+	tweet := Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Some big polar bears here at the north pole today"}
+	if err := json.NewEncoder(w).Encode(tweet); err != nil {
+		panic(err)
+	}
+}
+
+// TweetCreate creates a new tweet and returns it.
+func TweetCreate(w http.ResponseWriter, r *http.Request) {
+	todos := Tweets{
+		Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Some big polar bears here at the north pole today"},
+		Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Saw over 5000 penguins, nothing new to report."},
+	}
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+}
+
+//TweetUpdate updates a tweet.
+func TweetUpdate(w http.ResponseWriter, r *http.Request) {
+	tweet := Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Some big polar bears here at the north pole today"}
+	if err := json.NewEncoder(w).Encode(tweet); err != nil {
+		panic(err)
+	}
+}
+
+//TweetDelete updates a tweet.
+func TweetDelete(w http.ResponseWriter, r *http.Request) {
+	tweet := Tweet{UserName: "CodeWorkshop", Created: time.Now(), Content: "Some big polar bears here at the north pole today"}
+	if err := json.NewEncoder(w).Encode(tweet); err != nil {
+		panic(err)
+	}
+}
+
+/*
+Register handlers with the router.
+*/
+// NewRouter returns a mux router for our app.
+func NewRouter() *mux.Router {
+	// Create a new mux router
+	r := mux.NewRouter()
+	//Register an endpoint with the router to list the tweets.
+	r.HandleFunc("/tweets", TweetList).
+		Methods("GET")
+	r.HandleFunc("/tweets/{id}", TweetDetail).
+		Methods("GET")
+	r.HandleFunc("/tweets", TweetList).
+		Methods("PUT")
+	r.HandleFunc("/tweets/{id}", TweetDetail).
+		Methods("POST")
+	r.HandleFunc("/tweets/{id}", TweetDelete).
+		Methods("DELETE")
+	return r
+}
+
+/*
+Tweet model.
+*/
+// Tweet represents a tweet
+type Tweet struct {
+	UserName string    `json:"name"`
+	Content  string    `json:"content"`
+	Created  time.Time `json:"created"`
+}
+
+// Tweets represents a list of Tweets.
+type Tweets []Tweet
